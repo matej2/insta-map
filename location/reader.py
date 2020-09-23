0a1d6cb5f5f0a4836c1b65ee3d1079265b324c89
Matej J <11059438+matej2@users.noreply.github.com>
2020-09-23 22:13:20 +0200

    Fetch posts from multiple locations

--- location/reader.py
+++ location/reader.py
@@ -1,14 +1,43 @@
 import json
 import os
+import random
+from time import sleep
+
 import requests as r
 
 from insta_map.settings import BASE_DIR
+from wall.test.tests import parse_location, parse_posts, MapData, create_test_json, City, Location
+
+random.seed()
 
 
 def get_json():
-    dir = os.path.join(__file__, "test", "locations.json")
+    dir = os.path.join(os.getcwd(), "test", "locations.json")
     url = f''
+    c = 0
+    arr = []
+
     with open(dir, "r") as file:  # Use file to refer to the file object
         data = file.read()
         obj = json.loads(data)
-        r.get()
+        l = obj["location_list"]
+
+        for i in obj["location_list"]:
+
+            res = r.get(f'https://www.instagram.com/explore/locations/{i["id"]}/ljubljana-slovenia/?__a=1').json()
+            posts = parse_posts(res["graphql"])
+            lat = res["graphql"]["location"]["lat"]
+            lng = res["graphql"]["location"]["lng"]
+            name = res["graphql"]["location"]["name"]
+            loc = Location(i["id"], lat, lng, name)
+
+            arr.append(MapData(loc, posts))
+
+            print(res)
+            if c >= 1:
+                break
+            c = c + 1
+            sleep(random.random()*10)
+    create_test_json(arr)
+
+get_json()
