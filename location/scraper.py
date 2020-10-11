c328db4f2f4dd341b296e00b51f283eedde946c6
Matej J <11059438+matej2@users.noreply.github.com>
2020-10-11 22:26:35 +0200

    Location id

--- location/scraper.py
+++ location/scraper.py
@@ -33,7 +33,7 @@ def scrape_locations():
                         l.id = r["id"]
                     l.city = d
                     l.name = r["name"][:254]
-                    l.url = 'https://www.instagram.com/explore/locations/' + d.id
+                    l.url = 'https://www.instagram.com/explore/locations/' + r["id"]
                     l.save()
 
                     st = st + 1
