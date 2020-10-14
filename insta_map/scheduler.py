f3134807b3b7a9dced0a5e5d8dd1846e2c8421cf
matej2 <11059438+matej2@users.noreply.github.com>
2020-10-14 15:17:44 +0200

    Fixed bug where settigns are not loading

--- insta_map/scheduler.py
+++ insta_map/scheduler.py
@@ -5,18 +5,17 @@ import django
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 
-from location.scraper import scrape_locations
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "insta_map.settings")
 django.setup()
 
+from location.scraper import scrape_locations
 from photo.scraper import scrape_photos
 
 if __name__ == '__main__':
     scheduler = AsyncIOScheduler()
 
-    scheduler.add_job(scrape_locations, trigger=IntervalTrigger(days=6))
-    scheduler.add_job(scrape_photos, trigger=IntervalTrigger(days=1))
+    scheduler.add_job(scrape_locations, trigger=IntervalTrigger(days=5))
+    scheduler.add_job(scrape_photos, trigger=IntervalTrigger(hours=5))
 
     scheduler.start()
     # Execution will block here until Ctrl+C (Ctrl+Break on Windows) is pressed.
