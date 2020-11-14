1daf00922e8d1fc1e56f0a791a03c122d1549143
Matej J <11059438+matej2@users.noreply.github.com>
2020-11-14 01:32:15 +0100

    Attempt to try prod bug2

--- insta_map/settings.py
+++ insta_map/settings.py
@@ -12,16 +12,12 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import dj_database_url
-import dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import django_heroku
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-dotenv_file = os.path.join(BASE_DIR, ".env")
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
